ff38f3408e6cdc4cf95697d375918fed14d5124f
Heri Prasetyo <45659342+Heri-Prasetyo@users.noreply.github.com>
2018-12-22 10:17:14 +0700

    Add files via upload

---
+++ PROGRAM MENGHITUNG GAJI SEORANG PEGAWAI JIKA PERJAM 5000.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+    int main(){
+    int Jam;
+
+    cout<<"PROGRAM MENGHITUNG GAJI SEORANG PEGAWAI"<<endl;
+
+    cout<<"Masukan Berapa Jam Pegawai bekerja : ";
+    cin>>Jam;
+    Jam=Jam*5000;
+    cout<<"Gaji yang di peroleh Pegawai       : "<<Jam;
+    return 0;
+
+
+
+    }
